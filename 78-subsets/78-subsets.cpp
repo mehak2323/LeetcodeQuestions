802cb6922e28abf3c5d39552230211782ef4c28b
Mehak Khatri <mehakkhatri10@gmail.com>
2022-04-17 06:46:31 -0400

    Time: 0 ms (100.00%), Space: 15.9 MB (20.17%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    //My recursion function
+    void findSubsets(int i, vector<int> valsf ,vector<int> &nums, vector<vector<int>> &ans)
+    {
+        //base case, if all traversed, add answer and return
+        if(i<0)
+        {
+            ans.push_back(valsf);
+            return;
+        }
+        
+        //find subset without adding current element
+        findSubsets(i-1, valsf, nums, ans);
+        //add current element
+        valsf.push_back(nums[i]);
+        //find subsets after adding current element
+        findSubsets(i-1, valsf, nums, ans);
+    }
+    
+    //Given
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        vector<vector<int>> ans;
+        vector<int> valsf;
+        int n = nums.size();
+        
+        findSubsets(n-1,valsf,nums,ans);
+        
+        return ans;
+    }
+};
