fa94d2800f4516d2f954fcaf3f1ad2ca9f907c6e
Mehak Khatri <mehakkhatri10@gmail.com>
2022-05-27 01:45:18 -0400

    Time: 0 ms (100.00%), Space: 5.9 MB (74.11%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,10 +1,28 @@
 class Solution {
 public:
     int numberOfSteps(int num) {
+        
+        int steps=0;
+        
+        while(num>0){
+            if(num%2==0)
+                num/=2;
+            else
+                num-=1;
+            
+            steps++;
+        }
+        
+        return steps;
+    }
+    
+    /*
+        int numberOfSteps(int num) {
         if(num==0)
             return 0;
         if(num%2==0)
             return 1 + numberOfSteps(num/2);
         return 1 + numberOfSteps(num-1);
     }
+    */
 };
